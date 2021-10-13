3cbd78f206a6fa8b7b13f5a622cd1b2a201cbf8d
Pyunghyuk Yoo <yoophi@gmail.com>
2021-10-13 20:23:14 +0900

    ✨ NEW: add mock endpoints for todos

--- nest-todos-typeorm/src/todos/todos.controller.ts
+++ nest-todos-typeorm/src/todos/todos.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateTodoDto } from './dto/create-todo';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  @Get()
+  getTodos() {
+    return [];
+  }
+
+  @Get(':todoId')
+  getTodoDetail(@Param('todoId') todoId: string) {
+    return {
+      id: todoId,
+      title: 'sample todo',
+      completed: false,
+    };
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto) {
+    return {
+      id: uuid(),
+      ...createTodoDto,
+    };
+  }
+
+  @Delete(':todoId')
+  deleteTodo(@Param('todoId') todoId: string) {
+    return {};
+  }
+}
